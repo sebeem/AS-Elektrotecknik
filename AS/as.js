eddcc40e1ec2a07188d4c9b57de899dea1d9e329
MIlan991 <81048118+MIlan991@users.noreply.github.com>
2021-10-03 22:25:27 +0200

    Add files via upload

--- AS/as.js
+++ AS/as.js
@@ -1,1 +1,15 @@
-//In progress
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3.5,
+  spaceBetween: 100,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".arrow-right",
+    prevEl: ".arrow-left"
+  }
+});
